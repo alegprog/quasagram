494dddd1400863c49640454edfbf913ef466eb17
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-09-21 16:16:08 -0400

    Add some basic logic

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+    <input type="text" v-model="message" @keyup="handleKeyup" />
+    <button type="button" @click="clearMessage">Clear</button>
+    <h5>{{ message }}</h5>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      message: 'I love Vuejs!',
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = '';
+    },
+    handleKeyup(e) {
+      if (e.keyCode === 27) { // Esc key
+        this.clearMessage();
+      } else if(e.keyCode === 13) { // Enter key
+        this.alertMessage();
+      }
+    },
+    alertMessage() {
+      alert(this.message);
+    }
+  }
 }
 </script>
