e6ddc8322460536aa29e147ec9a281b1e8e6e0db
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-09-23 10:14:32 -0400

    Add getters and a child component Task

--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -1,9 +1,24 @@
 const state = {
-  tasks: [
-    { id: 1, name: 'Go to shop', completed: false, dueDate: '2019-05-12', dueTime: '18:30' },
-    { id: 2, name: 'Get bananas', completed: false, dueDate: '2019-05-13', dueTime: '15:30' },
-    { id: 3, name: 'Get apples', completed: false, dueDate: '2019-05-14', dueTime: '12:30' },
-  ]
+  tasks: {
+    'ID1': {
+      name: 'Go to shop',
+      completed: false,
+      dueDate: '2019-05-12',
+      dueTime: '18:30'
+    },
+    'ID2': {
+      name: 'Get bananas',
+      completed: false,
+      dueDate: '2019-05-13',
+      dueTime: '15:30'
+    },
+    'ID3': {
+      name: 'Get apples',
+      completed: false,
+      dueDate: '2019-05-14',
+      dueTime: '12:30'
+    }
+  }
 };
 
 const mutations = {
