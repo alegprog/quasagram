4b92cbca7e952ad311c9c497dda4feec686b1359
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-09-23 23:52:11 -0400

    Make more reusable components wih sync, $emit, $refs of $refs

--- src/components/Tasks/Modals/AddTask.vue
+++ src/components/Tasks/Modals/AddTask.vue
@@ -3,60 +3,23 @@
     <modal-header>Add Task</modal-header>
     <form @submit.prevent="submitForm">
       <q-card-section>
-        <modal-task-name :name.sync="taskToSubmit.name" />
-        <div class="row q-mb-sm">
-          <q-input outlined label="Due Date" v-model="taskToSubmit.dueDate" class="col">
-            <template v-slot:append>
-              <q-icon
-                v-if="taskToSubmit.dueDate"
-                name="close"
-                @click="clearDueDate"
-                class="cursor-pointer" />
-              <q-icon name="event" class="cursor-pointer">
-                <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-                  <q-date v-model="taskToSubmit.dueDate">
-                    <div class="row items-center justify-end">
-                      <q-btn v-close-popup label="Close" color="primary" flat />
-                    </div>
-                  </q-date>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
-          </q-input>
-        </div>
 
-        <div
+        <modal-task-name
+          ref="modalTaskName"
+          :name.sync="taskToSubmit.name" />
+
+        <modal-due-date
+          :dueDate.sync="taskToSubmit.dueDate"
+          @clear="clearDueDate" />
+
+        <modal-due-time
           v-if="taskToSubmit.dueDate"
-          class="row q-mb-sm"
-        >
-          <q-input
-            outlined
-            label="Due Time"
-            v-model="taskToSubmit.dueTime"
-            class="col">
-            <template v-slot:append>
-              <q-icon
-                v-if="taskToSubmit.dueTime"
-                name="close"
-                @click="taskToSubmit.dueTime = ''"
-                class="cursor-pointer" />
-              <q-icon name="access_time" class="cursor-pointer">
-                <q-popup-proxy transition-show="scale" transition-hide="scale">
-                  <q-time v-model="taskToSubmit.dueTime">
-                    <div class="row items-center justify-end">
-                      <q-btn v-close-popup label="Close" color="primary" flat />
-                    </div>
-                  </q-time>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
-          </q-input>
-        </div>
+          :dueTime.sync="taskToSubmit.dueTime" />
+
       </q-card-section>
 
-      <q-card-actions align="right">
-        <q-btn type="submit" label="Save" color="primary" />
-      </q-card-actions>
+      <modal-buttons />
+
     </form>
   </q-card>
 </template>
@@ -67,6 +30,9 @@ export default {
   components: {
     'modal-header': require('components/Tasks/Modals/Shared/ModalHeader').default,
     'modal-task-name': require('components/Tasks/Modals/Shared/ModalTaskName').default,
+    'modal-due-date': require('components/Tasks/Modals/Shared/ModalDueDate').default,
+    'modal-due-time': require('components/Tasks/Modals/Shared/ModalDueTime').default,
+    'modal-buttons': require('components/Tasks/Modals/Shared/ModalButtons').default,
   },
   data() {
     return {
@@ -81,8 +47,8 @@ export default {
   methods: {
     ...mapActions('tasks', ['addTask']),
     submitForm() {
-      this.$refs.name.validate();
-      if (!this.$refs.name.hasError) {
+      this.$refs.modalTaskName.$refs.name.validate();
+      if (!this.$refs.modalTaskName.$refs.name.hasError) {
         this.submitTask();
       }
     },
