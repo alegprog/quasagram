2ea84206cb93eab65a174d4bd9f80f58a3f263f0
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-09-25 09:02:08 -0400

    Add posts layout and mini profile

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -26,7 +26,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-page-container>
+    <q-page-container class="bg-grey-1">
       <router-view />
     </q-page-container>
 
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,11 +1,114 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Page</h5>
+  <q-page class="constrain q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat bordered
+          >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">danny_connell</q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-img
+            :src="post.imageUrl"
+          />
+
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+          </q-card-section>
+
+        </q-card>
+      </div>
+      <div class="col-4">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">danny_connell</q-item-label>
+            <q-item-label caption>
+              Danny Connel
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { date } from 'quasar';
+
+const posts = [
+  {
+    id: 1,
+    caption: 'Golden Gate Bridge',
+    date: 1601036874567,
+    location: 'San Francisco, United States',
+    imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+  },
+  {
+    id: 2,
+    caption: 'Golden Gate Bridge',
+    date: 1601036874567,
+    location: 'San Francisco, United States',
+    imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+  },
+  {
+    id: 3,
+    caption: 'Golden Gate Bridge',
+    date: 1601036874567,
+    location: 'San Francisco, United States',
+    imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+  },
+  {
+    id: 4,
+    caption: 'Golden Gate Bridge',
+    date: 1601036874567,
+    location: 'San Francisco, United States',
+    imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+  },
+];
+
 export default {
-  name: 'PageHome'
+  name: 'PageHome',
+  data() {
+    return {
+      posts: posts,
+    }
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, 'MMMM D, h:mmA');
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+.card-post {
+  .q-img {
+    min-height: 12.5rem;
+  }
+}
+</style>
