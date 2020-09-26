187600f29a15c041166d27e075312e0cceb27038
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-09-26 14:28:37 -0400

    Add a nodejs backend with heroku

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,31 @@
 // dependencies
+/*jshint esversion: 6 */
 
-const express = require('express')
+const express = require('express');
 
 // configs
 
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Endpoints
 
-app.get('/', (request, response) => {
-  response.send('I love nodejs 23!')
-})
+app.get('/posts', (request, response) => {
+  let posts = [
+    {
+      caption: 'Golden Gate Bridge',
+      location: 'San Francisco'
+    },
+    {
+      caption: 'London Eye',
+      location: 'London'
+    },
+  ];
+  response.send(posts);
+});
 
 // listen
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
