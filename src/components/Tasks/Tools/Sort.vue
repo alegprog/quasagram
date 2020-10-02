acf74e3c16ecb2023e7d146ae769ee8052ee5830
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-10-02 10:03:55 -0400

    Sort dropdown Configure options

--- src/components/Tasks/Tools/Sort.vue
+++ src/components/Tasks/Tools/Sort.vue
@@ -2,8 +2,10 @@
   <q-select
     filled
     class="col q-ml-sm"
-    v-model="model"
+    v-model="sortBy"
     :options="options"
+    emit-value
+    map-options
     label="Sort by"
     stack-label
   />
@@ -12,10 +14,16 @@
 export default {
   data () {
     return {
-      model: null,
-
+      sortBy: null,
       options: [
-        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+        {
+          label: 'Name',
+          value: 'name'
+        },
+        {
+          label: 'Date',
+          value: 'dueDate'
+        },
       ],
     }
   }
