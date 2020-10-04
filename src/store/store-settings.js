9e1cb8defc1da6c238963b8a711b2039e7f9ecfe
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-10-04 00:05:11 -0400

    Get & apply the LocalStorage Settings on App Load

--- src/store/store-settings.js
+++ src/store/store-settings.js
@@ -10,6 +10,7 @@ const state = {
 const mutations = {
   setShow12HourTimeFormat: (state, value) => state.settings.show12HourTimeFormat = value,
   setShowTasksInOneList: (state, value) => state.settings.showTasksInOneList = value,
+  setSettings: (state, settings) => Object.assign(state.settings, settings),
 };
 
 const actions = {
@@ -21,8 +22,12 @@ const actions = {
     commit('setShowTasksInOneList', value);
     dispatch('saveSettings');
   },
-  saveSettings: ({state}) => {
-    LocalStorage.set('settings', state.settings);
+  saveSettings: ({ state }) => LocalStorage.set('settings', state.settings),
+  getSettings: ({ commit }) => {
+    let settings = LocalStorage.getItem('settings');
+    if (settings) {
+      commit('setSettings', settings);
+    }
   },
 };
 
