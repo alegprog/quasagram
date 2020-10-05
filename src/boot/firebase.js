000c4c08ca8d19a69e9d1358e986f61d37d3c648
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-10-05 06:46:54 -0400

    Add firebase with boot file and an auth store

---
+++ src/boot/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdl-cjpanhdV7tpr5ZbHnS81SJelmQ0u0",
+  authDomain: "awesome-todo-f0655.firebaseapp.com",
+  databaseURL: "https://awesome-todo-f0655.firebaseio.com",
+  projectId: "awesome-todo-f0655",
+  storageBucket: "awesome-todo-f0655.appspot.com",
+  messagingSenderId: "300235121064",
+  appId: "1:300235121064:web:0e384c146121e76bba3d9d"
+};
+
+let firebaseApp = firebase.initializeApp(firebaseConfig);
+
+let firebaseAuth = firebaseApp.auth();
+
+export { firebaseAuth };
