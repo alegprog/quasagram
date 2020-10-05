e5a1b853b072e2af4cd2ed6793791b6639a850d2
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-10-05 16:09:48 -0400

    Move keys to env vars

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdl-cjpanhdV7tpr5ZbHnS81SJelmQ0u0",
-  authDomain: "awesome-todo-f0655.firebaseapp.com",
-  databaseURL: "https://awesome-todo-f0655.firebaseio.com",
-  projectId: "awesome-todo-f0655",
-  storageBucket: "awesome-todo-f0655.appspot.com",
-  messagingSenderId: "300235121064",
-  appId: "1:300235121064:web:0e384c146121e76bba3d9d"
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FB_DB_URL,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_ID,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FB_APP_ID
 };
 
 let firebaseApp = firebase.initializeApp(firebaseConfig);
