2092f625cc4604a0078e83e0d1863b7b4bd7ca6f
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-10-05 21:10:03 -0400

    Fix style electron

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -89,9 +89,16 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .q-scroll-area-tasks {
   display: flex;
   flex-grow: 1;
+}
+.electron {
+  .q-scroll-area-tasks {
+    .scroll {
+      height: auto !important;
+    }
+  }
 }
 </style>
