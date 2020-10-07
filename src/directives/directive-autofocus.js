54e123a674a68cc54588ab0e2a4c6ef0394319c9
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-10-06 22:47:33 -0400

    Limit the focus delay to only cordova platform

--- src/directives/directive-autofocus.js
+++ src/directives/directive-autofocus.js
@@ -1,8 +1,12 @@
+import { Platform } from 'quasar';
+
 export const autofocus = {
   inserted(el) {
-    let input = el.querySelector('.q-field__native');
+    const input = el.querySelector('.q-field__native');
+    let delay = 0;
+    if (Platform.is.cordova) delay = 700;
     setTimeout(() => {
       input.focus();
-    }, 700);
+    }, delay);
   }
 };
